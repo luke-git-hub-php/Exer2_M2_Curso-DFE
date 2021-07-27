b7698e0f38d7ddddaa356462d6514ae33eae22f8
luke-git-hub-php <lucasvieiradasilvajw@gmail.com>
2021-07-26 21:15:31 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,194 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { sortBy } from 'lodash';
+import './App.css'; 
+ 
+const DEFAULT_QUERY = '';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = 20; 
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-function App() {
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'author'),
+  URL: list => sortBy(list, 'url').reverse(),
+  POINTS: list => sortBy(list, 'points').reverse(),
+};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: null,
+      query: DEFAULT_QUERY,
+      page: DEFAULT_PAGE,
+      searchKey: '',
+      isLoading: false,
+      sortKey: 'NONE',
+      isSortReverse: false,
+    };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+    this.onSort = this.onSort.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey } = this.state;
+    const oldHits = page === 0 ? [] : this.state.results[searchKey].hits;
+    const updatedHits = [ ...oldHits, ...hits ];
+    this.setState({ results: { ...this.state.results, [searchKey]: { hits: updatedHits, page } }, isLoading: false });
+  }
+
+  fetchSearchTopStories(query, page) {
+    this.setState({ isLoading: true })
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result));
+  }
+
+  onSearchChange(event) {
+    this.setState({ query: event.target.value, page: 0 });
+  }
+
+  onSearchSubmit(event) {
+    const { query, page } = this.state;
+    this.setState({ searchKey: query });
+    if (this.needsToSearchTopStories(query)) {
+      this.fetchSearchTopStories(query, page);
+    }
+    event.preventDefault();
+  }
+
+  needsToSearchTopStories(query) {
+    return !this.state.results[query];
+  }
+
+  onSort(sortKey) {
+    const isSortReverse = sortKey === this.state.sortKey ? true : false
+    this.setState({ sortKey: sortKey, isSortReverse: isSortReverse });
+  }
+
+  componentDidMount() {
+    const { query, page } = this.state;
+    this.setState({ searchKey: query });
+    this.fetchSearchTopStories(query, page);
+  }
+
+  render() {
+    const { results, query, searchKey, isLoading, sortKey, isSortReverse } = this.state;
+    const page = (results && results[searchKey]) ? results[searchKey].page : 0;
+    const list = (results && results[searchKey]) ? results[searchKey].hits : [];
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search value={query} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
+            <i className="fa fa-search" aria-hidden="true"></i>
+          </Search>
+        </div>
+        <Table list={list} sortKey={sortKey} onSort={this.onSort} isSortReverse={isSortReverse} />
+        <div className="interactions">
+          <ButtonWithLoading
+            isLoading={isLoading}
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </ButtonWithLoading>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Loading = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <i className="fa fa-spinner" aria-hidden="true"></i>
     </div>
+  );
+};
+
+const Search = (props) => {
+  const { value, onChange, onSubmit, children } = props;
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange} placeholder="Digite um title, author ou Url" />
+      <button type="submit" Value="Consultar" >Pesquisar{children}</button>
+    </form>
+  );
+};
+
+const Table = (props) => {
+  const { list, sortKey, onSort, isSortReverse } = props;
+  const sortedList = SORTS[sortKey](list);
+  const reverseSortedList = isSortReverse ? SORTS[sortKey](list).reverse() : sortedList;
+  return (
+    <div className="table">
+      <div className="table-header">
+        <span style={{ width: '40%' }}>
+          <Sort sortKey={'TITLE'} onSort={onSort} activeSortKey={sortKey}>Title</Sort>
+        </span>
+        <span style={{ width: '45%' }}>
+          <Sort sortKey={'AUTHOR'} onSort={onSort} activeSortKey={sortKey}>Author</Sort>
+        </span>
+        <span style={{ width: '15%' }}>
+          <Sort sortKey={'URL'} onSort={onSort} activeSortKey={sortKey}>URL</Sort>
+        </span>
+      </div>
+    { reverseSortedList.map((item) =>
+        <div key={item.objectID} className="table-row">
+          <span style={{ width: '40%' }}><a href={item.url} className="subject">{item.title}</a></span>
+          <span style={{ width: '30%' }}>{item.author}</span>
+          <span style={{ width: '15%' }}>{item.url}</span>
+        </div>
+      ) }
+    </div>
+  );
+};
+
+const Sort = (props) => {
+  const { sortKey, onSort, children, activeSortKey } = props;
+  const sortClass = ["button-inline"];
+  if (sortKey === activeSortKey) {
+    sortClass.push("button-active");
+  }
+  return (
+    <Button className={sortClass.join(" ")} onClick={() => onSort(sortKey)}>
+      {children}
+    </Button>
+  );
+};
+
+const Button = (props) => {
+  const { onClick, children, className } = props;
+  return (
+    <button type="button" onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+};
+
+const withLoading = (Component) => ({ isLoading, ...props }) => {
+  return (
+    isLoading ? <Loading /> : <Component { ...props } />
   );
 }
 
+const ButtonWithLoading = withLoading(Button)
+
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import rendered from 'react-test-renderer';
+import { Search, Button, Table } from './App';
+  
+describe('Search', () => {
+ 
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+  });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test('snapshots', () => {
+    const component = rendered.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Button', () => {
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>More</Button>, div);
+  });
+
+  test('snapshots', () => {
+    const component = rendered.create(
+      <Button>More</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', url: 1},
+      { title: '2', author: '2', url: 1},
+    ],
+  };
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test('snapshots', () => {
+    const component = rendered.create(
+      <Table {...props} />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
 });
